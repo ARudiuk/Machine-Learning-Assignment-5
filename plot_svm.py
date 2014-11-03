a6229367d6ef68f1855c8f0b5dc95f449935ab4d
HsoulT <alxanderr@gmail.com>
2014-11-02 22:28:26 -0400

    Updated graph style

--- plot_svm.py
+++ plot_svm.py
@@ -8,11 +8,14 @@ y3 = errors[:,2].T
 y4 = errors[:,3].T
 y5 = errors[:,4].T
 y6 = errors[:,5].T
+print np.amax(errors)
 errors = np.load("svm_results_2.npy")
 y1 = np.append(y1,errors[2:,0].T)
 y2 = np.append(y2,errors[2:,1].T)
 y4 = np.append(y4,errors[2:,3].T)
+print np.amax(errors)
 errors = np.load("svm_results_train.npy")
+print np.amax(errors)
 x2 = np.arange(-5,6,1)
 y7 = errors[:,0].T
 y8 = errors[:,1].T
@@ -22,19 +25,21 @@ y11 = errors[:,4].T
 y12 = errors[:,5].T
 pl.plot(x,y1,label='Linear')
 pl.plot(x,y2,label='RBF')
-pl.plot(x2,y3,label='Sigmoid')
-pl.plot(x,y4,label='Poly Deg 2')
-pl.plot(x2,y5,label='Poly Deg 3')
-pl.plot(x2,y6,label='Poly Deg 4')
+# pl.plot(x2,y3,label='Sigmoid')
+# pl.plot(x,y4,label='Poly Deg 2')
+# pl.plot(x2,y5,label='Poly Deg 3')
+# pl.plot(x2,y6,label='Poly Deg 4')
 
-pl.plot(x2,y7,label='Linear Train')
-pl.plot(x2,y8,label='RBF Train')
-pl.plot(x2,y9,label='Sigmoid Train')
-pl.plot(x2,y10,label='Poly Deg 2 Train')
-pl.plot(x2,y11,label='Poly Deg 3 Train')
-pl.plot(x2,y12,label='Poly Deg 4 Train')
+# pl.plot(x2,y7,label='Linear Train')
+# pl.plot(x2,y8,label='RBF Train')
+# pl.plot(x2,y9,label='Sigmoid Train')
+# pl.plot(x2,y10,label='Poly Deg 2 Train')
+# pl.plot(x2,y11,label='Poly Deg 3 Train')
+# pl.plot(x2,y12,label='Poly Deg 4 Train')
 pl.legend(loc="center left")
 pl.xlabel("2^x = C")
 pl.ylabel("Percentage Correct")
 pl.title("Accuracy vs C value for SVM")
+pl.ylim(80,100)
+pl.xlim(-5,10)
 pl.show()
